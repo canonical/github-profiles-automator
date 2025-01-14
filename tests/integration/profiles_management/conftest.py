0e58d10f1de8722b97e04c99311430fd8fe66740
mvlassis <manos.vlassis@canonical.com>
2025-01-14 14:18:38 +0200

    Don't redeploy profiles-controller if it already exists

--- tests/integration/profiles_management/conftest.py
+++ tests/integration/profiles_management/conftest.py
@@ -28,6 +28,11 @@ async def deploy_profiles_controller(ops_test: OpsTest):
     if not ops_test.model:
         pytest.fail("ops_test has a None model", pytrace=False)
 
+    log.info(ops_test.model.applications)
+    if PROFILES_CHARM in ops_test.model.applications:
+        log.info("Profiles Controller charm already exists, no need to re-deploy")
+        return
+        
     log.info("Deploying the Profiles Controller charm.")
     await ops_test.model.deploy(PROFILES_CHARM, channel=PROFILES_CHANNEL, trust=PROFILES_TRUST)
 
--- tests/integration/profiles_management/test_delete_stale_profiles.py
+++ tests/integration/profiles_management/test_delete_stale_profiles.py
@@ -6,7 +6,7 @@ from lightkube import Client
 from profiles_management.delete_stale import delete_stale_profiles
 from profiles_management.helpers.profiles import list_profiles
 from profiles_management.pmr import classes
-from tests.integration.profiles_management.helpers import k8s, profiles
+from tests.integration.profiles_management.helpers import profiles
 
 log = logging.getLogger(__name__)
 client = Client(field_manager="profiles-automator-lightkube")
@@ -22,18 +22,12 @@ async def test_delete_stale_profiles(deploy_profiles_controller, lightkube_clien
     context = {"namespace": namespace}
 
     profile_path = TESTS_YAMLS_PATH + "/profile.yaml"
-    contributor_path = TESTS_YAMLS_PATH + "/contributor.yaml"
 
     # Load and apply all objects from files
     profile_contents = profiles.load_profile_from_file(profile_path, context)
-    resources = k8s.load_namespaced_objects_from_file(contributor_path, context)
 
     log.info("Creating Profile and waiting for Namespace to be created...")
     profiles.apply_profile(profile_contents, lightkube_client)
-
-    log.info("Applying all namespaced contributor resources.")
-    for resource in resources:
-        lightkube_client.apply(resource)
 
     # Create the PMR, which should not contain the above test profile
     pmr = classes.ProfilesManagementRepresentation()
