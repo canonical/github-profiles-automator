c91a8700cfb887028a1ba4df3317d8af4fb92cc8
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-01-17 19:13:56 +0200

    wip

--- src/profiles_management/helpers/kfam.py
+++ src/profiles_management/helpers/kfam.py
@@ -1,8 +1,9 @@
 """Utility module for manipulating KFAM resources."""
 
 import logging
-from typing import List
+from typing import List, TypeVar
 
+from charmed_kubeflow_chisme.lightkube.batch import delete_many
 from lightkube import Client
 from lightkube.generic_resource import GenericNamespacedResource, create_namespaced_resource
 from lightkube.resources.rbac_authorization_v1 import RoleBinding
@@ -18,6 +19,7 @@ AuthorizationPolicy = create_namespaced_resource(
     kind="AuthorizationPolicy",
     plural="authorizationpolicies",
 )
+KFAMResource = TypeVar("KFAMResource", GenericNamespacedResource, RoleBinding)
 
 
 def has_kfam_annotations(resource: GenericNamespacedResource | RoleBinding) -> bool:
@@ -97,16 +99,18 @@ def get_contributor_role(
     return classes.ContributorRole(annotations["role"])
 
 
-def rolebinding_matches_profile_contributor(rb: RoleBinding, profile: classes.Profile) -> bool:
+def resource_matches_profile_contributor(
+    resource: RoleBinding | GenericNamespacedResource, profile: classes.Profile
+) -> bool:
     """Check if the user and it's role in the RoleBinding match the PMR."""
     if profile.contributors is None:
         return False
 
-    if not has_kfam_annotations(rb):
+    if not has_kfam_annotations(resource):
         return False
 
-    role = get_contributor_role(rb)
-    user = get_contributor_user(rb)
+    role = get_contributor_role(resource)
+    user = get_contributor_user(resource)
     for contributor_role in profile._contributors_dict.get(user, []):
         if contributor_role == role:
             return True
@@ -138,6 +142,51 @@ def generate_contributor_rolebinding(
             "subjects": [
                 {"apiGroup": "rbac.authorization.k8s.io", "kind": "User", "name": contributor.name}
             ],
+        },
+    )
+
+
+def generate_contributor_authorization_policy(
+    contributor: classes.Contributor,
+    namespace: str,
+    kfp_ui_principal: str,
+    istio_ingressgateway_principal: str,
+) -> GenericNamespacedResource:
+    """Generate AuthorizatioinPolicy for a PMR Contributor."""
+    name_rfc1123 = k8s.to_rfc1123_compliant(f"{contributor.name}-{contributor.role}")
+
+    return AuthorizationPolicy.from_dict(
+        {
+            "metadata": {
+                "name": name_rfc1123,
+                "namespace": namespace,
+                "annotations": {
+                    "user": contributor.name,
+                    "role": contributor.role,
+                },
+            },
+            "spec": {
+                "rules": [
+                    {
+                        "from": [
+                            {
+                                "source": {
+                                    "principals": [
+                                        kfp_ui_principal,
+                                        istio_ingressgateway_principal,
+                                    ]
+                                }
+                            }
+                        ],
+                        "when": [
+                            {
+                                "key": "request.headers[kubeflow-userid]",
+                                "values": [contributor.name],
+                            }
+                        ],
+                    }
+                ],
+            },
         },
     )
 
@@ -196,3 +245,115 @@ def list_contributor_authorization_policies(
         for ap in authorization_policies
         if has_kfam_annotations(ap) and not resource_is_for_profile_owner(ap)
     ]
+
+
+def delete_resources_not_matching_profile_contributors(
+    client: Client,
+    profile: classes.Profile,
+    existing_resources: List[KFAMResource],
+) -> List[KFAMResource]:
+    """Delete RoleBindings or AuthorizationPolicies in the cluster that doesn't match PMR.
+
+    Returns:
+        The remaining resources, after removing the deleted ones from the existing_resources.
+    """
+    resources_to_delete = []
+    remaining_resources = []
+
+    for resource in existing_resources:
+        if not resource_matches_profile_contributor(resource, profile):
+            log.info(
+                "Resource '%s' doesn't belong to Profile. Will delete it.",
+                k8s.get_name(resource),
+            )
+            resources_to_delete.append(resource)
+        else:
+            remaining_resources.append(resource)
+
+    log.info("Deleting all resources that don't match the PMR.")
+    delete_many(client, resources_to_delete, logger=log)
+
+    return remaining_resources
+
+
+def kfam_resources_list_to_roles_dict(
+    resources: List[KFAMResource],
+) -> dict[str, List[classes.ContributorRole]]:
+    """Convert list of KFAM RoleBindings or AuthorizationPolicies to dict.
+
+    For keys the user of the resource will be used and as a value the role of the resource.
+
+    Args:
+        resources: List of KFAM RoleBindings or AuthorizationPolicies.
+
+    Returns:
+        Dictionary with keys the user names and values the roles, derived from parsing all
+        the provided resources.
+    """
+    contributor_roles_dict = {}
+    for resource in resources:
+        if has_kfam_annotations(resource):
+            user = get_contributor_user(resource)
+            role = get_contributor_role(resource)
+            contributor_roles_dict[user] = contributor_roles_dict.get(user, []) + [role]
+
+    return contributor_roles_dict
+
+
+def create_rolebindings_for_profile_contributors(
+    client: Client,
+    profile: classes.Profile,
+    existing_rolebindings: List[RoleBinding],
+):
+    """Create RoleBindings for all contributors defined in a Profile, in the PMR.
+
+    If a RoleBinding already exists for the specific Contributor name and role, then
+    no API requests will happen.
+
+    Args:
+        client: The lightkube client to use.
+        profile: The PMR to iterate over its Contributors.
+        existing_rolebindings: List of existing RoleBindings, to avoid doing reduntant
+                               API requests
+    """
+    existing_contributor_roles = kfam_resources_list_to_roles_dict(existing_rolebindings)
+
+    for contributor in profile.contributors or []:
+        if contributor.role not in existing_contributor_roles.get(contributor.name, []):
+            log.info("Will create RoleBinding for Contributor: %s", contributor)
+            client.apply(generate_contributor_rolebinding(contributor, profile.name))
+
+
+def create_authorization_policies_for_profile_contributors(
+    client: Client,
+    profile: classes.Profile,
+    existing_authorization_policies: List[GenericNamespacedResource],
+    kfp_ui_principal: str,
+    istio_ingressgateway_principal: str,
+):
+    """Create RoleBindings for all contributors defined in a Profile, in the PMR.
+
+    If a RoleBinding already exists for the specific Contributor name and role, then
+    no API requests will happen.
+
+    Args:
+        client: The lightkube client to use.
+        profile: The PMR to iterate over its Contributors.
+        existing_authorization_policies: List of existing AuthorizationPolicies, to avoid doing
+                                         reduntant API requests.
+        kfp_ui_principal: The Istio principal of KFP UI, based on the ServiceAccount, to use
+                          when creating AuthorizationPolicies for Contributors.
+        istio_ingressgateway_principal: The Istio principal of IngressGateway, based on the
+                                        ServiceAccount, to use when creating AuthorizationPolicies
+                                        for Contributors.
+    """
+    existing_contributor_roles = kfam_resources_list_to_roles_dict(existing_authorization_policies)
+
+    for contributor in profile.contributors or []:
+        if contributor.role not in existing_contributor_roles.get(contributor.name, []):
+            log.info("Will create AuthorizationPolicy for Contributor: %s", contributor)
+            client.apply(
+                generate_contributor_authorization_policy(
+                    contributor, profile.name, kfp_ui_principal, istio_ingressgateway_principal
+                )
+            )
