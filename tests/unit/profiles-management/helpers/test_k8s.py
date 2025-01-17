cdbc5011ff2038a2423e5ed5f21dc31861d92a0f
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-01-17 13:01:50 +0200

    Unit tests

---
+++ tests/unit/profiles-management/helpers/test_k8s.py
@@ -0,0 +1,31 @@
+from lightkube.generic_resource import GenericNamespacedResource
+from lightkube.models.meta_v1 import ObjectMeta
+
+from profiles_management.helpers.k8s import get_annotations, to_rfc1123_compliant
+
+
+def test_annotations():
+    resource = GenericNamespacedResource(
+        metadata=ObjectMeta(name="test", annotations={"test": "value"})
+    )
+
+    assert get_annotations(resource) == {"test": "value"}
+
+
+def test_no_annotations():
+    resource = GenericNamespacedResource(metadata=ObjectMeta(name="test"))
+
+    assert get_annotations(resource) == {}
+
+
+def test_non_alphanum_to_hyphen():
+    name = "kimonas@canonical.com"
+
+    assert to_rfc1123_compliant(name) == "kimonas-canonical-com"
+
+
+def test_more_than_63_char_name():
+    name = "I-had-to-think-of-a-reeeally-long-string-to-use-for-the-test-which-was-tough"
+
+    assert len(name) > 63
+    assert len(to_rfc1123_compliant(name)) == 63
--- tests/unit/profiles-management/helpers/test_kfam.py
+++ tests/unit/profiles-management/helpers/test_kfam.py
@@ -1,8 +1,17 @@
 import pytest
 from lightkube.generic_resource import GenericNamespacedResource
 from lightkube.models.meta_v1 import ObjectMeta
+from lightkube.models.rbac_v1 import RoleRef
+from lightkube.resources.rbac_authorization_v1 import RoleBinding
 
-from profiles_management.helpers.kfam import has_kfam_annotations, resource_is_for_profile_owner
+from profiles_management.helpers.kfam import (
+    get_contributor_role,
+    get_contributor_user,
+    has_kfam_annotations,
+    resource_is_for_profile_owner,
+    resource_matches_profile_contributor,
+)
+from profiles_management.pmr.classes import Contributor, ContributorRole, Owner, Profile, UserKind
 
 
 def test_kfam_resource():
@@ -11,6 +20,14 @@ def test_kfam_resource():
     )
 
     assert has_kfam_annotations(resource)
+
+
+def test_wrong_kfam_role_annotation():
+    resource = GenericNamespacedResource(
+        metadata=ObjectMeta(name="test", annotations={"role": "overlord", "user": "test"})
+    )
+
+    assert not has_kfam_annotations(resource)
 
 
 def test_non_kfam_resource():
@@ -34,3 +51,62 @@ def test_non_profile_owner_resource():
     resource = GenericNamespacedResource(metadata=ObjectMeta(name="random"))
 
     assert not resource_is_for_profile_owner(resource)
+
+
+def test_contributor_getters():
+    user = "test"
+    role = ContributorRole.ADMIN
+    resource = GenericNamespacedResource(
+        metadata=ObjectMeta(name="test", annotations={"role": role, "user": user})
+    )
+
+    assert get_contributor_user(resource) == user
+    assert get_contributor_role(resource) == role
+
+
+def test_rolebinding_not_matching_empty_profile_contributors():
+    rb = RoleBinding(
+        metadata=ObjectMeta(annotations={"user": "test", "role": "admin"}),
+        roleRef=RoleRef(apiGroup="", kind="", name=""),
+    )
+
+    profile = Profile(
+        name="test",
+        owner=Owner(name="test", kind=UserKind.USER),
+        contributors=[],
+        resources={},
+    )
+
+    assert not resource_matches_profile_contributor(rb, profile)
+
+
+def test_rolebinding_not_matching_profile_contributors():
+    rb = RoleBinding(
+        metadata=ObjectMeta(annotations={"user": "test", "role": "admin"}),
+        roleRef=RoleRef(apiGroup="", kind="", name=""),
+    )
+
+    profile = Profile(
+        name="test",
+        owner=Owner(name="test", kind=UserKind.USER),
+        contributors=[Contributor(name="test", role=ContributorRole.VIEW)],
+        resources={},
+    )
+
+    assert not resource_matches_profile_contributor(rb, profile)
+
+
+def test_rolebinding_matching_profile_contributors():
+    rb = RoleBinding(
+        metadata=ObjectMeta(annotations={"user": "test", "role": "admin"}),
+        roleRef=RoleRef(apiGroup="", kind="", name=""),
+    )
+
+    profile = Profile(
+        name="test",
+        owner=Owner(name="test", kind=UserKind.USER),
+        contributors=[Contributor(name="test", role=ContributorRole.ADMIN)],
+        resources={},
+    )
+
+    assert resource_matches_profile_contributor(rb, profile)
