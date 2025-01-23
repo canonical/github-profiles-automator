babe4d5519685dfcac3dacbbac47d0d825733ab4
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-01-23 09:24:22 +0000

    review: Parametrise annotation unit tests

--- tests/unit/profiles-management/helpers/test_k8s.py
+++ tests/unit/profiles-management/helpers/test_k8s.py
@@ -5,18 +5,20 @@ from lightkube.models.meta_v1 import ObjectMeta
 from profiles_management.helpers.k8s import get_annotations, to_rfc1123_compliant
 
 
-def test_annotations():
-    resource = GenericNamespacedResource(
-        metadata=ObjectMeta(name="test", annotations={"test": "value"})
-    )
-
-    assert get_annotations(resource) == {"test": "value"}
-
-
-def test_no_annotations():
-    resource = GenericNamespacedResource(metadata=ObjectMeta(name="test"))
-
-    assert get_annotations(resource) == {}
+@pytest.mark.parametrize(
+    "resource,expected_annotations",
+    [
+        (
+            GenericNamespacedResource(
+                metadata=ObjectMeta(name="test", annotations={"test": "value"})
+            ),
+            {"test": "value"},
+        ),
+        (GenericNamespacedResource(metadata=ObjectMeta(name="test")), {}),
+    ],
+)
+def test_annotations(resource, expected_annotations):
+    assert get_annotations(resource) == expected_annotations
 
 
 @pytest.mark.parametrize(
